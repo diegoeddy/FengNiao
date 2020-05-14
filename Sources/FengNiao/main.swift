f23abb0debb87585c257ca44967f97396f56309e
frank <4538404+diegoeddy@users.noreply.github.com>
2020-05-14 15:57:30 +0800

    扫描结束直接生成结果,不询问下一步操作

--- Sources/FengNiao/main.swift
+++ Sources/FengNiao/main.swift
@@ -144,6 +144,15 @@ if unusedFiles.isEmpty {
     exit(EX_OK)
 }
 
+let size = unusedFiles.reduce(0) { $0 + $1.size }.fn_readableSize
+print("\(unusedFiles.count) unused files are found. Total Size: \(size)".yellow.bold)
+
+for file in unusedFiles.sorted(by: { $0.size > $1.size }) {
+    print("\(file.readableSize) \(file.path.string)")
+}
+exit(EX_OK)
+
+
 if !isForce {
     var result = promptResult(files: unusedFiles)
     while result == .list {
